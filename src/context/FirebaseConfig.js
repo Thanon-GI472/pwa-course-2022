3b9f61e2c537f6d73121bc29e7ec6bf5874c8e46
Thanon-GI472 <thanon.than@bumail.net>
2022-04-23 17:26:08 +0700

    hosting Firebase

--- src/context/FirebaseConfig.js
+++ src/context/FirebaseConfig.js
@@ -2,14 +2,14 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/database';
 const firebaseConfig = {
-  apiKey: "AIzaSyDOPJ9_X7MbrMXHkai922aLK2NVa7xbGjo",
-  authDomain: "gi472-pwa-course-2022.firebaseapp.com",
-  projectId: "gi472-pwa-course-2022",
-  storageBucket: "gi472-pwa-course-2022.appspot.com",
-  messagingSenderId: "967644458652",
-  appId: "1:967644458652:web:6f71ad83773786006b8759",
-  measurementId: "G-Z7Z74R200B",
-  databaseURL: "https://gi472-pwa-course-2022-default-rtdb.asia-southeast1.firebasedatabase.app"
+  apiKey: "AIzaSyADAFAzpP19wRdyQSH1a4HMC3mRnA1nttY",
+  authDomain: "gi472week14.firebaseapp.com",
+  projectId: "gi472week14",
+  storageBucket: "gi472week14.appspot.com",
+  messagingSenderId: "166580429742",
+  appId: "1:166580429742:web:1f30ac23f045929b901e3f",
+  measurementId: "G-QYPND00LN9",
+  databaseURL: "https://gi472week14-default-rtdb.asia-southeast1.firebasedatabase.app"
 };
 
 firebase.initializeApp(firebaseConfig);
